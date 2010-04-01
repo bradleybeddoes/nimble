7ef40def55a8f66a11ec018cc63334ead033e760
cdotyone <cdoty1@yahoo.com>
2010-03-31 23:22:44 -0500

    fixes problem with createTip function
    also removes type in messages-nimble.properties

--- web-app/js/jquery/nimbleui.js
+++ web-app/js/jquery/nimbleui.js
@@ -110,14 +110,16 @@ $(function() {
 });
 
 function createTip(id,tle,msg) {
-    $("#"+id).after(
-            '<div id="'+id+'_tip">'+
-                '<div class="ui-dialog ui-widget">'+
-                '<div class="ui-dialog-titlebar ui-widget-header ui-corner-all"><strong>'+tle+'</strong></div>'+
-                '<div class="ui-dialog-content ui-widget-content">'+msg+'</div>'+
-            '</div></div>');
-    $("#"+id+"_tip").hide();
-    $("#"+id).bt({contentSelector: $("#"+id+"_tip"), width: '350px', closeWhenOthersOpen: true, shrinkToFit: 'true', positions: ['right', 'top', 'left'], margin: 0, padding: 6, fill: '#fff', strokeWidth: 1, strokeStyle: '#c2c2c2', spikeGirth: 12, spikeLength:9, hoverIntentOpts: {interval: 100, timeout: 1000}});
+    $(function() {
+        $(document.body).after(
+                '<div id="'+id+'_tip">'+
+                    '<div class="ui-dialog ui-widget">'+
+                    '<div class="ui-dialog-titlebar ui-widget-header ui-corner-all"><strong>'+tle+'</strong></div>'+
+                    '<div class="ui-dialog-content ui-widget-content">'+msg+'</div>'+
+                '</div></div>');
+        $("#"+id+"_tip").hide();
+        $("#"+id).bt({contentSelector: $("#"+id+"_tip"), width: '350px', closeWhenOthersOpen: true, shrinkToFit: 'true', positions: ['right', 'top', 'left'], margin: 0, padding: 6, fill: '#fff', strokeWidth: 1, strokeStyle: '#c2c2c2', spikeGirth: 12, spikeLength:9, hoverIntentOpts: {interval: 100, timeout: 1000}});
+    });
 }
 
 function createTabs(id) {
